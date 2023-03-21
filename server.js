d5f697405899eff5f8e27d94f6d71eee373c99f4
Jimmy Yin <118097335+xKranze@users.noreply.github.com>
2023-03-20 22:12:10 -0700

    started server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const inquirer = require("inquirer");
+const mysql = require('mysql2');
+
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password
+      password: '',
+      database: 'employees_db'
+    },
+    console.log(`Connected to the employees_db database.`)
+  );
+  
